6372fb7c34fb4197638d23fe5cd0bc4e9996d15d
Patrick Schadler <patrick.schadler@gmail.com>
2020-04-15 12:26:13 +0200

    feat(order-summary): add total price

--- src/components/container/BurgerBuilder/BurgerBuilder.js
+++ src/components/container/BurgerBuilder/BurgerBuilder.js
@@ -93,6 +93,7 @@ class BurgerBuilder extends Component {
           modalClosed={this.purchaseCancelHandler}
         >
           <OrderSummary
+            price={this.state.totalPrice}
             ingredients={this.state.ingredients}
             purchaseCancel={this.purchaseCancelHandler}
             purchaseContinued={this.purchaseContinueHandler}
