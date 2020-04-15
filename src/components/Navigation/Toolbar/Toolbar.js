5bfcef8ebc5ea9635a4d0a780fd31c5342d35a29
Patrick Schadler <patrick.schadler@gmail.com>
2020-04-15 12:40:57 +0200

    feat(toolbar): adding a toolbar

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Toolbar.module.css';
+
+const toolbar = (props) => (
+  <header className={classes.Toolbar}>
+    <div>MENU</div>
+    <div>LOGO</div>
+    <div>
+      <nav>...</nav>
+    </div>
+  </header>
+);
+
+export default toolbar;
